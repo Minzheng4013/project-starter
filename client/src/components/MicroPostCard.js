1db55e89121d18c2b6b98d14f6f4694caf9ea15e
VincenzoCammilleri2 <cammilleriv2@gmail.com>
2022-11-22 19:36:32 -0500

    trying to implement a timestamp for when each post is actually posted

--- client/src/components/MicroPostCard.js
+++ client/src/components/MicroPostCard.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+//import {Date} from "prismic-react"
+//import { RichText, Date } from 'react-router-dom';
+
 
 //create micropostcard.css to style each card
 function MicroPostCard({ content, createdAt, id }) {
   return (
+
+    createdAt= new Date(),
+    //createdAt = Date(document.data.event_date).toString(),
+
     <div className="col-10 col-md-8 col-lg-7">
       <div className="card mb-4 shadow">
         <div className="card-body card-text color:black">
           <div className="card">
-          <Link to={"/posts/" + id}>{content}</Link>
+          <p to={"/posts/" + id}>{content}</p>
           </div>
         </div>
-        <div className="card-footer small text-muted text-end">{createdAt}</div>
+        <div className="card-footer small text-muted text-end">{createdAt.toLocaleString()/*createdAt*/}</div>
       </div>
     </div>
   );
